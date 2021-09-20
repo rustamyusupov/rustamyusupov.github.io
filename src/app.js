a97a2f86e59f301f3bf0936c623a7468c1d0b4d3
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-09-20 11:13:04 +0200

    refactor(src): format app and styles by prettier

--- src/app.js
+++ src/app.js
@@ -1,22 +1,27 @@
-import wishes from '../public/wishes.json'
+import wishes from '../public/wishes.json';
 
 const visibilityMap = {
   a: 'archive',
   h: 'hidden',
-}
+};
 
-const byCategory = id => ({ categoryId }) => categoryId === id;
-const byVisibility = status => ({ visibility }) => visibility ? status.includes(visibility) : true;
+const byCategory =
+  id =>
+  ({ categoryId }) =>
+    categoryId === id;
+const byVisibility =
+  status =>
+  ({ visibility }) =>
+    visibility ? status.includes(visibility) : true;
 
 const getPrice = (amount, currency) =>
-  Math.ceil(amount).toLocaleString("ru", {
-    style: "currency",
+  Math.ceil(amount).toLocaleString('ru', {
+    style: 'currency',
     currency,
-    maximumFractionDigits: 0
+    maximumFractionDigits: 0,
   });
 
-const getWish = ({ name, link, price, currency, status }) =>
-  `<li class="wish">
+const getWish = ({ name, link, price, currency, status }) => `<li class="wish">
     <a
       class="${status}"
       href="${link}"
@@ -27,33 +32,35 @@ const getWish = ({ name, link, price, currency, status }) =>
     </a>
     &nbsp;&mdash; 
     <span>${getPrice(price, currency)}</span>
-  </li>`
+  </li>`;
 
-const render = ({ wishes, visibility }) => {
-  const { categories, items } = wishes;
-  const html = categories.map(({ id, name }) => {
-    const title = `<h4 class="category-title">${name}</h4>`;
-    const list = items
-      .filter(byCategory(id))
-      .filter(byVisibility(visibility))
-      .map(getWish)
-      .join('');
+const render = ({ lists, visibility }) => {
+  const { categories, items } = lists;
+  const html = categories
+    .map(({ id, name }) => {
+      const title = `<h4 class="category-title">${name}</h4>`;
+      const list = items
+        .filter(byCategory(id))
+        .filter(byVisibility(visibility))
+        .map(getWish)
+        .join('');
 
-    return `<div class="category">${title}<ul>${list}</ul></div>`;
-  }).join('');
+      return `<div class="category">${title}<ul>${list}</ul></div>`;
+    })
+    .join('');
 
-  document.querySelector("#root").innerHTML = html;
+  document.querySelector('#root').innerHTML = html;
 };
 
-const getQuery = search => {  
+const getQuery = search => {
   const urlSearchParams = new URLSearchParams(search);
 
   return Object.fromEntries(urlSearchParams.entries());
-}
+};
 
 document.addEventListener('DOMContentLoaded', () => {
   const { v = '' } = getQuery(window.location.search);
   const visibility = v.split(',').map(k => visibilityMap[k]);
 
-  render({ wishes, visibility });
+  render({ lists: wishes, visibility });
 });
